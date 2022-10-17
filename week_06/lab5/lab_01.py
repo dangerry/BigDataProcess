05040ab64c1f79cb98976176d108cd39d1c005bd
dangerry <chlekdms412@naver.com>
2022-10-17 20:49:33 +0900

    lab_01

---
+++ week_06/lab5/lab_01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+rainbow_dict = {'빨간색' : 'red', '주황색' : 'orange',
+                '노란색' : 'yellow', '초록색' : 'green',
+                '파란색' : 'blue', '남색' : 'navy',
+                '보라색' : 'purple'}
+
+print(rainbow_dict['초록색'])
