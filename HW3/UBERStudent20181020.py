ee934d134b1c4932143026bf2145cf00fc6bf1cf
dangerry <chlekdms412@naver.com>
2022-10-28 21:20:52 +0900

    Uber Info Count

---
+++ HW3/UBERStudent20181020.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import sys
+from datetime import date
+
+weekday = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+uber = dict()
+
+try:
+    with open(sys.argv[1], "rt") as df:
+        data = df.read()
+        rows = data.split("\n")
+
+        for row in rows:
+            info = row.split(",")
+            d = list(map(int, info[1].split("/")))
+            day = weekday[date(d[2], d[0], d[1]).weekday()]
+            s = info[0] + "," + day
+
+            if s not in uber:
+                uber[s] = info[2] + "," + info[3]
+            else:
+                v_t = uber.get(s).split(",")
+                vehicles = int(v_t[0]) + int(info[2])
+                trips = int(v_t[1]) + int(info[3])
+                uber[s] = str(vehicles) + "," + str(trips)
+
+except FileNotFoundError as e:
+    print("File Not Found")
+
+with open(sys.argv[2], "wt") as wf:
+    for i, j in zip(uber.keys(), uber.values()):
+        wf.write("%s %s\n" % (i, j))
