d4185f5e8fb7b66e0289097b81e752129df2f4e0
dangerry <chlekdms412@naver.com>
2022-10-31 00:50:13 +0900

    dict update

--- HW3/UBERStudent20181020.py
+++ HW3/UBERStudent20181020.py
@@ -4,8 +4,8 @@ import sys
 from datetime import date
 
 weekday = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
-info = []; values = []
-uber = dict()
+info = []; v = []; t = []
+uber_v = dict(); uber_t = dict()
 
 try:
     with open(sys.argv[1], "rt") as df:
@@ -17,22 +17,23 @@ try:
             d = list(map(int, element[1].split("/")))
             day = weekday[date(d[2], d[0], d[1]).weekday()]
             info.append(element[0] + "," + day)
-            values.append(element[2] + "," + element[3])
+            v.append(int(element[2]))
+            t.append(int(element[3]))
 
 except FileNotFoundError as e:
     print("File Not Found")
 
-for i, v in zip(info, values):
-    vehicles = 0; trips = 0
-    if i not in uber:
-        uber[i] = v
-    elif i in uber:
-        vt_values = list(map(int, v.split(",")))
-        vt_uber = list(map(int, uber.get(i).split(",")))
-        vehicles = vt_uber[0] + vt_values[0]
-        trips = vt_uber[1] + vt_values[1]
-        uber[i] = str(vehicles) + "," + str(trips)
+for i, v_info, t_info in zip(info, v, t):
+    if i not in uber_v:
+        uber_v[i] = v_info
+    else:
+        uber_v[i] += v_info
+    if i not in uber_t:
+        uber_t[i] = t_info
+    else:
+        uber_t[i] += t_info
+
 
 with open(sys.argv[2], "wt") as wf:
-    for i, j in zip(uber.keys(), uber.values()):
-        wf.write("%s %s\n" % (i, j))
+    for i, j, k in zip(uber_v.keys(), uber_v.values(), uber_t.values()):
+        wf.write("%s %d,%d\n" % (i, j, k))
